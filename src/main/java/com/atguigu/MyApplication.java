713e6d36dccc6dc47fac0e10e09224211d2ce6eb
lizheqi <lz2230867392@gmail.com>
2020-09-05 09:17:37 +0800

    mmmmm

--- src/main/java/com/atguigu/MyApplication.java
+++ src/main/java/com/atguigu/MyApplication.java
@@ -10,5 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class,args);
+        System.out.println("mmmmm");
     }
 }
